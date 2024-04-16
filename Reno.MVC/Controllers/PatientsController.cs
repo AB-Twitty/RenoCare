ac9b2ef1c1d360e3e714ccf6426b1dec1b77f60c
Abdelrahman  Fawzy <abdelrahmanfawzy003@gmail.com>
2024-04-16 23:12:03 +0200

    adding dataTable jquery plugin and building a method to get patients list

---
+++ Reno.MVC/Controllers/PatientsController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Reno.MVC.Services.Base;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reno.MVC.Controllers
+{
+	public class PatientsController : Controller
+	{
+		private readonly IClient _client;
+
+		public PatientsController(IClient client)
+		{
+			_client = client;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Index([FromQuery] int page = 1)
+		{
+			var list = await _client.PatientsAsync(page, new GetPatientListQueryRequest
+			{
+				PageIndex = page,
+				PageSize = 20
+			});
+
+			return View(list);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Index()
+		{
+			int totalRecord = 0;
+			int filterRecord = 0;
+			var draw = Request.Form["draw"].FirstOrDefault();
+			var sortColumn = Request.Form["columns[" + Request.Form["order[0][column]"].FirstOrDefault() + "][name]"].FirstOrDefault();
+			var sortColumnDirection = Request.Form["order[0][dir]"].FirstOrDefault();
+			var searchValue = Request.Form["search[value]"].FirstOrDefault();
+			int pageSize = Convert.ToInt32(Request.Form["length"].FirstOrDefault() ?? "0");
+			int skip = Convert.ToInt32(Request.Form["start"].FirstOrDefault() ?? "0");
+
+			int page = skip / pageSize + 1;
+			var data = (await _client.PatientsAsync(page, new GetPatientListQueryRequest
+			{
+				PageIndex = page,
+				PageSize = pageSize
+			})).Data;
+
+			//get total count of data in table
+			totalRecord = data.TotalCount;
+			// search data when search value found
+			if (!string.IsNullOrEmpty(searchValue))
+			{
+				data.Items = data.Items.Where(x => x.FirstName.ToLower().Contains(searchValue.ToLower()) || x.LastName.ToLower().Contains(searchValue.ToLower()));
+			}
+			// get total count of records after search
+			filterRecord = data.Items.Count();
+			//sort data
+			//if (!string.IsNullOrEmpty(sortColumn) && !string.IsNullOrEmpty(sortColumnDirection)) data.Items = data.Items.OrderBy(sortColumn + " " + sortColumnDirection);
+			//pagination
+
+			var items = data.Items.Select(x => new
+			{
+				PatientName = x.FirstName + " " + x.LastName,
+				ReportsSameUnit = x.ReportsCount,
+				ReportsOverral = x.ReportsCount,
+				x.Gender,
+				x.Age,
+				x.Diabetes,
+				x.Hypertension,
+				Smoking = "Non-Smoker"
+			}).ToArray();
+
+
+			var returnObj = new
+			{
+				draw = draw,
+				recordsTotal = totalRecord,
+				recordsFiltered = totalRecord,
+				data = items
+			};
+
+			return Ok(returnObj);
+		}
+	}
+
+}
+
---
+++ Reno.MVC/wwwroot/js/pages/PatientsIndex.js
@@ -0,0 +1,28 @@
+﻿$('document').ready(function() {
+    var table = $('#PatientTable').dataTable({
+        pageLength: 25,
+        ordering: true,
+        scrollX: true,
+        ajax: {
+            url: "/Patients",
+            type: "POST",
+            datatype: "json"
+        },
+        serverSide: true,
+        filter: true,
+        processing: true,
+        language: {
+            processing: "Processing, Please Wait..."
+        },
+        columns: [
+            { data: "patientName", name: "PatientName" },
+            { data: "reportsSameUnit", name: "ReportsSameUnit" },
+            { data: "reportsOverral", name: "ReportsOverral" },
+            { data: "gender", name: "Gender" },
+            { data: "age", name: "Age" },
+            { data: "diabetes", name: "Diabetes" },
+            { data: "hypertension", name: "Hypertension" },
+            { data: "smoking", name: "Smoking" }
+        ]
+    });
+});
