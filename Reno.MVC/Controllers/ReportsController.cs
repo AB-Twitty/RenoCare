adbf1afe32fa38a2b3916a5317fd3a53fb50badf
Abdelrahman  Fawzy <abdelrahmanfawzy003@gmail.com>
2024-05-12 17:19:47 +0300

    Create report UI design tabs

---
+++ Reno.MVC/Controllers/ReportsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Reno.MVC.Models.Reports;
+
+namespace Reno.MVC.Controllers
+{
+    public class ReportsController : Controller
+    {
+        public IActionResult Index(int patientId)
+        {
+            var model = new ReportIndexVM
+            {
+                PatientId = patientId,
+                PatientName = "Ahmed Ali",
+                ReportNephrologist = "Dr.Khaled Abdel-Ghaffar"
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ Reno.MVC/Models/Reports/ReportIndexVM.cs
@@ -0,0 +1,10 @@
+﻿namespace Reno.MVC.Models.Reports
+{
+    public class ReportIndexVM
+    {
+        public int PatientId { get; set; }
+        public string PatientName { get; set; }
+        public string ReportNephrologist { get; set; }
+
+    }
+}
